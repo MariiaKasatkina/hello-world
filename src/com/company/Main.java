53d5400d16e98679b831d2963afeb6a12de4ed08
Mariia Kasatkina <m.kass98@yandex.ru>
2020-04-03 23:34:56 +0300

    finish Hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //System.out.println("1.");
+        //System.out.println("2..");
+        //System.out.println("3...");
+        //System.out.println("Hello world!");
+
+        //int i = 1;
+        //System.out.println(i);
+        //i = i + 1;
+        //System.out.println(i);
+        //i = i + 1;
+        //System.out.println(i);
+
+        //int a = 1;
+        //int b = 2;
+        //int c = a + b;
+        //System.out.println("sum = " + c);
+
+        //int a = 1;
+        //int b = 2;
+        //System.out.println("a > b = " + (a > b));
+        //System.out.println("a < b = " + (a < b));
+
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
